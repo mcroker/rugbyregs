825b0bd45af3a5f6ba5b8e1e8640645b4107e3a3
Martin Croker <mcroker@gmail.com>
2022-11-23 13:53:07 +0000

    beta

---
+++ public/regfiles.js
@@ -0,0 +1,48 @@
+let regcache = {};
+
+function loadRegs(regUrl) {
+  return new Promise(function (resolve) {
+    loadRegFile(regUrl).then(function () {
+      resolve(parseRegCache(regcache['wr']));
+    });
+  });
+}
+
+function parseRegCache(reg) {
+  if (Array.isArray(reg.children)) {
+    return reg.children.flatMap(child => parseRegCache({ ...reg, children: null, ...child }));
+  } else {
+    return reg;
+  }
+}
+
+function loadRegFile(regUrl) {
+  return new Promise(function (resolve) {
+    if (undefined === regcache[regUrl]) {
+      $.getJSON(`./regs/${regUrl}.json`, function (data) {
+        const load = (Array.isArray(data.load)) ? data.load : [];
+        if ("string" === typeof data.parent) {
+          load.push(data.parent);
+        } else if (regUrl !== "wr") {
+          data.parent = "wr";
+          load.push(data.parent);
+        }
+        regcache[regUrl] = data;
+        Promise.all(load.map(i => loadRegFile(i)))
+          .then(i => {
+            if (undefined !== data.parent) {
+              if (Array.isArray(regcache[data.parent].children)) {
+                regcache[data.parent].children.push(data);
+              } else {
+                regcache[data.parent].children = [data];
+              }
+            }
+            resolve();
+          });
+      });
+    } else {
+      resolve();
+    }
+  })
+}
+
